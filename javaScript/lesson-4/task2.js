2b0653d4ad48052fb489393f44f52c1b1ab55389
nastjas <nastjasokolovaa@gmail.com>
2021-04-20 21:18:20 +0300

    Lesson-4

--- javaScript/lesson-4/task2.js
+++ javaScript/lesson-4/task2.js
@@ -5,36 +5,37 @@
 // 2.3. Перенести функционал подсчета корзины на объектно-ориентированную базу.
 
 const basketObj = {
-    струны: {
-        price: 1000,
-        quantity: 1
+    sum: 0,
+
+    basketList: {
+        струны: {
+            price: 1000,
+            quantity: 1
+        },
+        гитара: {
+            price: 15000,
+            quantity: 1
+        },
+        'набор канифоли': {
+            price: 500,
+            quantity: 1
+        },
+        'нотная тетрадь': {
+            price: 90,
+            quantity: 1
+        },
+        балалайка: {
+            price: 42000,
+            quantity: 1
+        }
     },
-    гитара: {
-        price: 15000,
-        quantity: 1
+
+    countBasketPrice() {
+        for (let key in this.basketList) {
+            this.sum += this.basketList[key].price * this.basketList[key].quantity
+        }
+        console.log(this.sum)
     },
-    'набор канифоли': {
-        price: 500,
-        quantity: 1
-    },
-    'нотная тетрадь': {
-        price: 90,
-        quantity: 1
-    },
-    балалайка: {
-        price: 42000,
-        quantity: 1
-    }
 }
 
-
-function countBasketPrice(obj) {
-    let sum = 0
-    let keys = Object.keys(obj)
-    for (let i = 0; i < keys.length; i++){
-        sum += ((obj[(keys[i])]).price * (obj[(keys[i])]).quantity)
-    }
-    console.log(sum)
-}
-
-countBasketPrice(basketObj)
+basketObj.countBasketPrice()
